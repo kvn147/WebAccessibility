3dba70795904147eeb94f61f6c85e844f1b1549a
kvn147 <kevin458377@gmail.com>
2024-04-24 23:24:01 -0700

    Added .txt file to test. Created places to store different results and getter for them, then toString method for output.

--- src/WebTest.java
+++ src/WebTest.java
@@ -1,3 +1,46 @@
+import java.util.*;
+
 public class WebTest {
+    private String category;
+    private String description;
+    private String googleResult;
+    private String waveResult;
+    private String sortSiteResult;
+    private String asLintResult;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getGoogleResult() {
+        return googleResult;
+    }
+
+    public String getWaveResult() {
+        return waveResult;
+    }
+
+    public String getSortSiteResult() {
+        return sortSiteResult;
+    }
+
+    public String getAsLintResult() {
+        return asLintResult;
+    }
+
+    public String toString() {
+        return "Keyboard: " + getCategory() + " Google: " + getGoogleResult() + " WAVE: " +
+                getWaveResult() + " SortSite: " + getSortSiteResult() + " ASLint: " +
+                getAsLintResult();
+        //Keyboard: Alert shows for a short time
+        // Google: notfound
+        // WAVE: notfound
+        // SortSite: notfound
+        // ASLint: notfound
+    }
 
 }
